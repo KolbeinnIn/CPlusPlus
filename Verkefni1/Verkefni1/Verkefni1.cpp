41ebf10731ee05007ba15356675e093f6fdf6aa1
KolbeinnIn <kolbeinningo@nemi.tskoli.is>
2018-08-21 16:43:08 +0000

    fyrsta prufa

---
+++ Verkefni1/Verkefni1/Verkefni1.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h";
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	int aldur;
+	cout << "Sláðu inn aldur: ";
+	cin >> aldur;
+	cout << "Þú ert " << aldur << "ára gamall\n";
+	return 0;
+}
