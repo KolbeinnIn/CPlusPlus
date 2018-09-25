f6c8f34e5cc0139cddda460f93bafbe956d229e2
KolbeinnIn <kolbeinningo@nemi.tskoli.is>
2018-09-25 17:13:30 +0000

    skil02 búið alveg sjálfur

--- Skil02/Skil02.cpp
+++ Skil02/Skil02.cpp
@@ -10,35 +10,82 @@
 using namespace std;
 
 
+void ip2fylki(int *ipTolur, int ipTala) {
+	ipTolur[0] = (ipTala >> 24) & 0xff;
+	ipTolur[1] = (ipTala >> 16) & 0xff;
+	ipTolur[2] = (ipTala >> 8) & 0xff;
+	ipTolur[3] = ipTala & 0xff;
+}
+
+void synaIpTolu(const char*texti, int *ipTolur){
+	for (int i = 0; i < 4; i++){
+		if (i != 3){
+			cout << ipTolur[i] << ".";
+		}
+		else{
+			cout << ipTolur[i] << "   ";
+		}
+	}
+}
+
+void synaIpTolu(const char*texti, int ipTala){
+	int ip[4];
+	ip2fylki(ip, ipTala);
+	synaIpTolu(texti, ip);
+}
+ 
+
+
+int ip2int(int *ipTolur){
+	return ipTolur[0] << 24 | ipTolur[1] << 16 | ipTolur[2] << 8 | ipTolur[3];	
+}
+
+void subnetting(int ipNet, int netBitar, int bitaFjoldi){
+	cout << "IP net        Fyrsta nothæfa Síðasta nothæfa Broadcast     Maski" << endl;
+	cout << "--------------------------------------------------------------------------" << endl;
+	int magic = pow(2, (32 - (netBitar + bitaFjoldi))); //magic number er fyrsti biti sem er 1 talinn frá hægri.
+	for (int i = 0; i < pow(2, bitaFjoldi); i++) {
+		int tempnet = ipNet + (magic*i);
+		synaIpTolu("", tempnet); //network
+		synaIpTolu("\t", tempnet + 1); //fyrsta nothæfa
+		synaIpTolu("\t", tempnet + magic - 2); //seinasta nothæfa
+		synaIpTolu("\t", tempnet + magic - 1); //broadcast
+		tempnet = 0xffffffff << (32 - (netBitar + bitaFjoldi)); //subnet maskinn vol.1
+		synaIpTolu("\t", tempnet); //subnet maskinn vol.2
+		cout << endl;
+	}
+
+}
+
 int main() {
-	int ipTala[4];
-	int ipHeiltolur[4];
-	int netBitar = 0;
-	int skipting = 0;
+	int *ipTolur = new int[4];
+
+	int prefix, subnet, subnetBitar; //subnet er hversu mörg net á að skipta í.
+	
 	//int subnet[4];
 	cout << "Sladu inn upphafsnet: ";
 	for (int i = 0; i < 4; i++){
-		cin >> ipTala[i];
+		cin >> ipTolur[i];
 	}
 	cout << endl << "Fjoldi netbita i upphafsneti: ";
-	cin >> netBitar;
+	cin >> prefix;
 	cout << endl << "Fjoldi neta sem a ad skipta upphafsnetinu i: ";
-	cin >> skipting;
+	cin >> subnet;
 
-
+	/*
 	for (int i = 0; i < 4; i++){
-		cout << ipTala[i];
+		cout << ipTolur[i];
 		if (i < 3){
 			cout << ".";
 		}
-	}
-	cout << endl;
-	ipHeiltolur[0] = ipTala[0] << 24;
-	ipHeiltolur[1] = ipTala[1] << 16;
-	ipHeiltolur[2] = ipTala[2] << 8;
-	ipHeiltolur[3] = ipTala[3];
-	unsigned int asd = ipTala[0] << 24 | ipTala[1] << 16 | ipTala[2] << 8 | ipTala[3]; //ATH UNSIGNED	
-	cout << asd;
+	}*/
+
+	cout << endl; 
+	
+	int ip2Dec = ip2int(ipTolur);
+	subnetBitar = ceil(log2(subnet));
+	subnetting(ip2Dec, prefix, subnetBitar); //ATH
+
 	/*
 	for (int i = 0; i < 32; i++){
 		if (i % 8 == 0 && i != 0){
