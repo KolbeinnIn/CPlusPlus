6283590c0ee629cef3c0d2aff68493a126a3419b
KolbeinnIn <kolbeinningo@nemi.tskoli.is>
2018-10-23 17:10:20 +0000

    asd

--- Skil04/Skil04.cpp
+++ Skil04/Skil04.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -7,29 +8,52 @@ class FlightBooking {
 public:
 	FlightBooking(int id, int capacity, int reserved);
 	void printStatus();
+	bool reserveSeats(int number_ob_seats);
+	bool canceReservations(int number_ob_seats);
 private:
 	int id;
 	int capacity;
 	int reserved;
 };
-void FlightBooking::printStatus()
-{
-	int perc = (double)reserved / capacity * 100;
-	cout << "Flight " << id << " : " << capacity << "/" << reserved << " (" << (perc) << "\%) seats reserved." << endl;
-}
-FlightBooking::FlightBooking(int id, int capacity, int reserved)
-{
+
+FlightBooking::FlightBooking(int id, int capacity, int reserved) {
 	this->id = id;
 	this->capacity = capacity;
 	this->reserved = reserved;
 }
+
+void FlightBooking::printStatus() {
+	int perc = (double)reserved / capacity * 100;
+	cout << "Flight " << id << " : " << reserved << "/" << capacity << " (" << (perc) << "\%) seats reserved." << endl;
+}
+
+bool FlightBooking::reserveSeats(int number_ob_seats) {
+	// try to add reservations and return 'true' on success keep the limits in mind  
+	return false;
+}
+bool FlightBooking::canceReservations(int number_ob_seats){
+
+	// try to cancel reservations and return 'true' on success keep the  limits in mind  
+	return false;
+}
+
 int main() {
-	int reserved = 0,capacity = 0;
-	std::cout << "Provide flight capacity: ";
-	std::cin >> capacity;
-	std::cout << "Provide number of reserved seats: ";
-	std::cin >> reserved;
+	int reserved = 0, capacity = 0, max = 0;
+	cout << "Provide flight capacity: ";
+	cin >> capacity;
+	max = round(capacity * 1.05);
+	while (reserved > max || reserved == 0){
+		cout << "Provide number of reserved seats: ";
+		cin >> reserved;
+	}	
+	
 	FlightBooking booking(1, capacity, reserved);
-	booking.printStatus();
+	string command = "";
+	while (command != "quit") {
+		booking.printStatus();
+		cout << "What would you like to do?: ";
+		cin >> command;
+		// handle the command  
+	}
 	return 0;
-}
+}
